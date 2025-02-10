3847f2b6d34105eaa78adbf9519c7d543a328a7b
Thierry Baraldi <thierrybaraldi@gmail.com>
2025-02-10 18:06:16 -0300

    adda classe usuario e instanciei o objeto usuario aula dia 10.02.25

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PrimeiroProjeto;
+
+Console.WriteLine("hello world");
+Usuario user1 = Usuario();
